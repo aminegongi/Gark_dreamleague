f4312c5d453d44862f3d096f7f6216e93d8cf483
achour <achourmedhedi@gmail.com>
2022-03-14 08:46:09 +0100

    salem

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-"# Gark_dreamleague" 
+# Isporit Website
+The official website of Isporit
+
+## Branches
+##### master (Stable branch)
+
+Linked to isporit.com
+
+
+## Run the website ( master ) with yarn
+
+1. yarn install
+2. yarn run dev 
--- firebase.config.js
+++ firebase.config.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDnHmNwR9J2T-VSNn9lnyKdG1kmznMfUcU',
-  authDomain: 'isporit.firebaseapp.com',
-  databaseURL: 'https://isporit.firebaseio.com',
-  projectId: 'isporit',
-  storageBucket: 'isporit.appspot.com',
-  messagingSenderId: '699215576608',
-  appId: '1:699215576608:web:ba02069dfbbb4f684f0645',
+  apiKey: 'AIzaSyA4Cz6ES-MFLRT2AkR24suPLE5Q_8hI1EM',
+  authDomain: 'achour-8acf1.firebaseapp.com',
+  databaseURL: 'https://achour-8acf1.firebaseio.com',
+  projectId: 'achour-8acf1',
+  storageBucket: 'achour-8acf1.appspot.com',
+  messagingSenderId: '433977748267',
+  appId: '1:433977748267:web:6039df93b62631d5eb336e',
 }
 
 // Initialize Firebase
+// initializeApp(firebaseConfig)
 initializeApp(firebaseConfig)
 
 export default getFirestore()
